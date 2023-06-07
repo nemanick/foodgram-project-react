111b90828b2ceeee81be6aa74148df8d95efc874
Иван Неманов <kadetivan2019@yandex.ru>
2023-06-08 00:36:12 +0700

    first try

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -58,11 +58,14 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
-
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
